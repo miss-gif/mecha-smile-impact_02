1acb39639b9a01e235b5c01c3143c324d7751403
miss-gif <ddc.akari@gmail.com>
2022-02-17 23:54:50 +0900

    add

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 new Swiper(".about .swiper-container", {
   // direction: 'horizontal', // 수평 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
+  // autoplay: true, // 자동 재생 여부
+  // loop: true, // 반복 재생 여부
   spaceBetween: 30, // 슬라이드 사이 여백
-  slidesPerView: 4, // 한 번에 보여줄 슬라이드 개수
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
   // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
   navigation: {
     // 슬라이드 이전/다음 버튼 사용 여부
@@ -14,8 +14,8 @@ new Swiper(".about .swiper-container", {
 
 new Swiper(".live .swiper-container", {
   // direction: 'horizontal', // 수평 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
+  // autoplay: true, // 자동 재생 여부
+  // loop: true, // 반복 재생 여부
   spaceBetween: 30, // 슬라이드 사이 여백
   slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
   // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
@@ -28,10 +28,10 @@ new Swiper(".live .swiper-container", {
 
 new Swiper(".exhibition .swiper-container", {
   // direction: 'horizontal', // 수평 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
+  // autoplay: true, // 자동 재생 여부
+  // loop: true, // 반복 재생 여부
   spaceBetween: 30, // 슬라이드 사이 여백
-  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
   // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
   navigation: {
     // 슬라이드 이전/다음 버튼 사용 여부
@@ -42,8 +42,8 @@ new Swiper(".exhibition .swiper-container", {
 
 new Swiper(".sales .swiper-container", {
   // direction: 'horizontal', // 수평 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
+  // autoplay: true, // 자동 재생 여부
+  // loop: true, // 반복 재생 여부
   spaceBetween: 30, // 슬라이드 사이 여백
   slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
   // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
