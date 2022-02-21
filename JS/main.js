42ca42935e31a96943c1fe1fad64910263efe633
miss-gif <ddc.akari@gmail.com>
2022-02-22 05:22:01 +0900

    add

--- JS/main.js
+++ JS/main.js
@@ -46,11 +46,25 @@ new Swiper(".sales .swiper-container", {
   loop: true, // 반복 재생 여부
   spaceBetween: 30, // 슬라이드 사이 여백
   slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
-  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  // slidesPerGroup: 2, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
   navigation: {
     // 슬라이드 이전/다음 버튼 사용 여부
     prevEl: ".sales .swiper-prev", // 이전 버튼 선택자
     nextEl: ".sales .swiper-next", // 다음 버튼 선택자
+  },
+});
+
+new Swiper(".gallery .swiper-container", {
+  // direction: 'horizontal', // 수평 슬라이드
+  // autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  slidesPerGroup: 3, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  navigation: {
+    // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: ".gallery .swiper-prev", // 이전 버튼 선택자
+    nextEl: ".gallery .swiper-next", // 다음 버튼 선택자
   },
 });
 
