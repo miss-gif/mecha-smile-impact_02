dbca83530e7f91da7679924860cce602bbb448dc
miss-gif <ddc.akari@gmail.com>
2022-02-26 15:21:07 +0900

    add

--- JS/main.js
+++ JS/main.js
@@ -100,24 +100,26 @@ let 전시전_주의사항_닫기버튼 = document.querySelector(".전시전_주
   전시전_주의사항.style.display = "none";
 });
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
+// //Get the button
+// let top버튼 = document.getElementById("top버튼");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+//     top버튼.style.display = "block";
+//     // 포트폴리오_공지사항_텍스트.style.display = "block";
+//   } else {
+//     top버튼.style.display = "none";
+//     // 포트폴리오_공지사항_텍스트.style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
