73cf8c289ebeccebdaeb6455cd602383c1f1fac4
miss-gif <ddc.akari@gmail.com>
2022-02-27 17:41:21 +0900

    add

--- JS/main.js
+++ JS/main.js
@@ -100,26 +100,26 @@ let 전시전_주의사항_닫기버튼 = document.querySelector(".전시전_주
   전시전_주의사항.style.display = "none";
 });
 
-// //Get the button
-// let top버튼 = document.getElementById("top버튼");
+let top버튼 = document.querySelector("#top버튼");
+let side_menu_bar = document.querySelector(".side_menu_bar");
 
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () {
-//   scrollFunction();
-// };
+window.onscroll = function () {
+  스크롤액션();
+};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-//     top버튼.style.display = "block";
-//     // 포트폴리오_공지사항_텍스트.style.display = "block";
-//   } else {
-//     top버튼.style.display = "none";
-//     // 포트폴리오_공지사항_텍스트.style.display = "none";
-//   }
-// }
+function 스크롤액션() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    top버튼.style.display = "flex";
+    포트폴리오_공지사항.style.display = "block";
+    side_menu_bar.style.display = "flex";
+  } else {
+    top버튼.style.display = "none";
+    포트폴리오_공지사항.style.display = "none";
+    side_menu_bar.style.display = "none";
+  }
+}
 
-// When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+top버튼.addEventListener("click", function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
